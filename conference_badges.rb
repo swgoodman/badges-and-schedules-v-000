03ad54e67d148e1936a72b03a7c649cd7c5a6b0f
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 20:15:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,8 +10,9 @@ def batch_badge_creator(name)
 end
 
 def assign_rooms(attendees)
+  room_assignments = []
   attendees.each_with_index do |name, index|
-    room_assignments = "Hello, #{name}! You'll be assigned to room #{index}!"
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{index}!"
   end
 end
 
