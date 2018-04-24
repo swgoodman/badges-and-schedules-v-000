4637b8f72b7d14794f6edadcbc9cb0fd3060e509
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 20:00:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name)
-  name.each do |name|
-    puts "Hello, my name is #{name}."
+  name.collect do |name|
+    "Hello, my name is #{name}."
   end
 end
