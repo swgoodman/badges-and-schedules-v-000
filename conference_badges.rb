2116fcc490ec1f438120845f4a2f999ffd066eb0
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 20:56:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,9 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  assign_rooms(attendees).each do |badge|
+  batch_badge_creator(attendees).each do |badge|
     puts "#{badge}"
   end
+  assign_rooms(attendees).each do |badge|
+    puts "#{badge}"
 end
